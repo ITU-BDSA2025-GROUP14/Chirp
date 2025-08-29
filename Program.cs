98e376b782b14f4a70ec832c323f23ba9f6ff266
Peter Raasthøj <praa@itu.dk>
2025-08-29 10:32:02 +0200

    Changed target framework from .net8 to .net9, added chirp functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+using (var reader = new StreamReader(@"C:\Users\peter\Documents\ITU\3. Semester\BDSA\Chirp.CLI\data\chirp_cli_db.csv"))
+{
+    var chirps = new ArrayList();
+    
+    while (!reader.EndOfStream)
+    {
+        chirps.Add(reader.ReadLine());
+    }
+
+    foreach (var chirp in chirps)
+    {
+        var input = chirp.ToString().Split(',');
+        Console.WriteLine(chirp);
+    }
+}
