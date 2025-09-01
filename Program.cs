72edd9cb3f90a4e4f5729519eb6f72a491915a91
Peter Raasthøj <praa@itu.dk>
2025-09-01 13:24:17 +0200

    Added functionality for writing to file via terminal arguments
    Co-authored-by: Christian Dam <cacd@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,24 +1,61 @@
 ﻿using System.Collections;
 using Microsoft.VisualBasic.FileIO;
 
-using (var parser = new TextFieldParser(@"C:\Users\peter\Documents\ITU\3. Semester\BDSA\Chirp.CLI\data\chirp_cli_db.csv"))
+public class Program
 {
-    var chirps = new ArrayList();
-    parser.TextFieldType = FieldType.Delimited;
-    parser.SetDelimiters(",");
-    
-    while (!parser.EndOfData)
+    public static void Main(string[] args)
     {
-        string[] fields = parser.ReadFields();
-        chirps.Add(fields);
+        var path = "data/chirp_cli_db.csv";
+        if (args.Length > 1)
+        {
+             if (args[0].ToLower().Equals("cheep"))
+             {
+                Cheep(path, args[1]);
+             }
+        }
+        else
+        {
+            Read(path);
+        }
     }
-    chirps.RemoveAt(0);
 
-    foreach (string[] chirp in chirps)
+    public static void Read(string path)
     {
-        var author      = chirp[0];
-        var message   = chirp[1];
-        var timestamp  = DateTimeOffset.FromUnixTimeSeconds(long.Parse(chirp[2]));
-        Console.WriteLine($"{author} @ {timestamp.LocalDateTime}: {message}");
+        using (var parser = new TextFieldParser(path))
+        {
+            var chirps = new ArrayList();
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+    
+            while (!parser.EndOfData)
+            {
+                string[] fields = parser.ReadFields();
+                chirps.Add(fields);
+            }
+            chirps.RemoveAt(0);
+
+            foreach (string[] chirp in chirps)
+            {
+                var author      = chirp[0];
+                var message   = chirp[1];
+                var timestamp  = DateTimeOffset.FromUnixTimeSeconds(long.Parse(chirp[2]));
+                Console.WriteLine($"{author} @ {timestamp.LocalDateTime}: {message}");
+            }
+        }
+    }
+    public static void Cheep(string path, string message)
+    {
+        if (!File.Exists(path))
+        {
+            using (var writer = new StreamWriter(path))
+            {
+                writer.WriteLine($"Author,Message,Timestamp");
+            }
+        }
+
+        using (var writer = File.AppendText(path))
+        {
+            writer.WriteLine($"{Environment.UserName},\"{message}\",{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}");
+        }
     }
 }
