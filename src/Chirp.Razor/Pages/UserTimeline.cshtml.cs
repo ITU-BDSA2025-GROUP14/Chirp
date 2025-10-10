34db2eb8332932ae25e79de7837bd28ee34d7549
Søren Bräuner <brae@itu.dk>
2025-10-10 11:53:54 +0200

    added pagination to UserTimeline page
    I added page query parameter binding with default value 1... also implemented the pagination logic with skip/take for displaying the 32 cheeps per page, and added pagination UI with the previous/next links as well as the page numbers.

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -8,14 +8,28 @@ public class UserTimelineModel : PageModel
     private readonly CheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
 
+    [BindProperty(SupportsGet = true)]
+    public int page { get; set; } = 1;
+    public int PageCount { get; set; }
+    public int PageSize { get; set; } = 32;
+    public string Author { get; set; }
+
+    public int TotalPages => (int)Math.Ceiling(decimal.Divide(PageCount, PageSize));
+
     public UserTimelineModel(CheepService service)
     {
         _service = service;
     }
 
+    public bool ShowPrevious => page > 1;
+    public bool ShowNext => page < TotalPages;
+
     public ActionResult OnGet(string author)
     {
-        Cheeps = _service.GetCheepsFromAuthor(author);
+        Author = author;
+        var allCheeps = _service.GetCheepsFromAuthor(author);
+        PageCount = allCheeps.Count;
+        Cheeps = allCheeps.Skip((page - 1) * PageSize).Take(PageSize).ToList();
         return Page();
     }
 }
